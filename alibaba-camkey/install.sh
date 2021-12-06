ddee0548e287bf4e1011273b6e10280d1f40270d
nnlabo <nnlabo@gmail.com>
2021-12-07 07:09:01 +0900

    change to swig3.0 add snowboy clone

--- alibaba-camkey/install.sh
+++ alibaba-camkey/install.sh
@@ -1,2 +1,4 @@
 sudo apt update
-sudo apt install swig4.0 python3-pyaudio sox libatlas-base-dev portaudio19-dev
+sudo apt install swig3.0 python3-pyaudio sox libatlas-base-dev portaudio19-dev
+cd
+git clone https://github.com/Kitt-AI/snowboy.git
